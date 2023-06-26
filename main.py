c05b4a6d52da2f0e1325fca688b6cedabf604092
AndranikSedrakyan <137534622+AndranikSedrakyan@users.noreply.github.com>
2023-06-26 17:27:17 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+#1
+
+#a = int(input('Enter length'))
+#b = int(input('Enter latitude'))
+#s = (a + b) * 2
+#print(s)
+
+#2
+
+#r = int(input('Enter radius'))
+#s = math.pi*(r**2)
+#print(s)
+
+#3
+
+#country = "Armenia"
+#city = "Erevan"
+#name = "Tiko"
+#lastname = "Hayrapetyan"
+#money = "dram"
+#enemy = "Turkey"
+#car = "Lamborghini"
+#brand = "Adidas"
+#gun = "gloke"
+
+#4
+
+#print(type("county"))
+#print(type("city"))
+#print(type("name"))
+#print(type("lastname"))
+#print(type("money"))
+#print(type("enemy"))
+#print(type("car"))
+#print(type("brand"))
+#print(type("gun"))
